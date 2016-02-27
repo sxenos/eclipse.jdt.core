04696d95d837cfa8c3caa5a38d7a4912e84398fe
Stefan Xenos <sxenos@gmail.com>
2016-02-26 22:26:05 -0800

    More wip on Regions
    Change-Id: I762e5de26f234033c9ab291accc92710a487d2ad

--- org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/Region.java
+++ org.eclipse.jdt.core/model/org/eclipse/jdt/internal/core/Region.java
@@ -11,9 +11,12 @@
 package org.eclipse.jdt.internal.core;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IParent;
 import org.eclipse.jdt.core.IRegion;
 
 /**
@@ -23,129 +26,188 @@ import org.eclipse.jdt.core.IRegion;
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class Region implements IRegion {
 
-	/**
-	 * A collection of the top level elements
-	 * that have been added to the region
-	 */
-	protected ArrayList rootElements;
-/**
- * Creates an empty region.
- *
- * @see IRegion
- */
-public Region() {
-	this.rootElements = new ArrayList(1);
-}
-/**
- * @see IRegion#add(IJavaElement)
- */
-public void add(IJavaElement element) {
-	if (!contains(element)) {
-		//"new" element added to region
-		removeAllChildren(element);
-		this.rootElements.add(element);
-		this.rootElements.trimToSize();
-	}
-}
-/**
- * @see IRegion
- */
-public boolean contains(IJavaElement element) {
+	private static final class Node {
+		private static Map<IJavaElement, Node> children = Collections.emptyMap();
 
-	int size = this.rootElements.size();
-	ArrayList parents = getAncestors(element);
-
-	for (int i = 0; i < size; i++) {
-		IJavaElement aTop = (IJavaElement) this.rootElements.get(i);
-		if (aTop.equals(element)) {
-			return true;
+		public Node() {
 		}
-		for (int j = 0, pSize = parents.size(); j < pSize; j++) {
-			if (aTop.equals(parents.get(j))) {
-				//an ancestor is already included
-				return true;
+
+		public void clearChildren() {
+			children = Collections.emptyMap();
+		}
+
+		public Node createChildFor(IJavaElement element) {
+			if (children.isEmpty()) {
+				children = new HashMap<>();
 			}
+
+			Node child = children.get(element);
+
+			if (child == null) {
+				child = new Node();
+				children.put(element, child);
+			}
+
+			return child;
 		}
-	}
-	return false;
-}
-/**
- * Returns a collection of all the parents of this element
- * in bottom-up order.
- *
- */
-private ArrayList getAncestors(IJavaElement element) {
-	ArrayList parents = new ArrayList();
-	IJavaElement parent = element.getParent();
-	while (parent != null) {
-		parents.add(parent);
-		parent = parent.getParent();
-	}
-	parents.trimToSize();
-	return parents;
-}
-/**
- * @see IRegion
- */
-public IJavaElement[] getElements() {
-	int size= this.rootElements.size();
-	IJavaElement[] roots= new IJavaElement[size];
-	for (int i = 0; i < size; i++) {
-		roots[i]= (IJavaElement) this.rootElements.get(i);
-	}
 
-	return roots;
-}
-/**
- * @see IRegion#remove(IJavaElement)
- */
-public boolean remove(IJavaElement element) {
+		public Node findChildFor(IJavaElement element) {
+			return children.get(element);
+		}
 
-	removeAllChildren(element);
-	return this.rootElements.remove(element);
-}
-/**
- * Removes any children of this element that are contained within this
- * region as this parent is about to be added to the region.
- *
- * <p>Children are all children, not just direct children.
- */
-protected void removeAllChildren(IJavaElement element) {
-	if (element instanceof IParent) {
-		ArrayList newRootElements = new ArrayList();
-		for (int i = 0, size = this.rootElements.size(); i < size; i++) {
-			IJavaElement currentRoot = (IJavaElement)this.rootElements.get(i);
-			//walk the current root hierarchy
-			IJavaElement parent = currentRoot.getParent();
-			boolean isChild= false;
-			while (parent != null) {
-				if (parent.equals(element)) {
-					isChild= true;
-					break;
+		public int countLeafNodes() {
+			if (isEmpty()) {
+				return 1;
+			}
+
+			int result = 0;
+			for (Node next : children.values()) {
+				result += next.countLeafNodes();
+			}
+			return result;
+		}
+
+		boolean isEmpty() {
+			return children.isEmpty();
+		}
+
+		public int gatherLeaves(IJavaElement[] result, int i) {
+			for (Map.Entry<IJavaElement, Node> next : children.entrySet()) {
+				Node nextNode = next.getValue();
+				if (nextNode.isEmpty()) {
+					result[i++] = next.getKey();
+				} else {
+					i = nextNode.gatherLeaves(result, i);
 				}
-				parent = parent.getParent();
 			}
-			if (!isChild) {
-				newRootElements.add(currentRoot);
-			}
+			return i;
 		}
-		this.rootElements= newRootElements;
-	}
-}
-/**
- * Returns a printable representation of this region.
- */
-public String toString() {
-	StringBuffer buffer= new StringBuffer();
-	IJavaElement[] roots= getElements();
-	buffer.append('[');
-	for (int i= 0; i < roots.length; i++) {
-		buffer.append(roots[i].getElementName());
-		if (i < (roots.length - 1)) {
-			buffer.append(", "); //$NON-NLS-1$
+
+		public void removeChild(IJavaElement currentElement) {
+			children.remove(currentElement);
 		}
 	}
-	buffer.append(']');
-	return buffer.toString();
-}
+
+	private Node root = new Node();
+
+	@Override
+	public void add(IJavaElement element) {
+		if (contains(element)) {
+			return;
+		}
+		Node node = createNodeFor(element);
+		node.clearChildren();
+	}
+
+	private Node createNodeFor(IJavaElement element) {
+		if (element == null) {
+			return this.root;
+		}
+
+		Node parentNode = createNodeFor(element.getParent());
+
+		return parentNode.createChildFor(element);
+	}
+
+	@Override
+	public boolean contains(IJavaElement element) {
+		Node existingNode = findMostSpecificNodeFor(element);
+
+		if (existingNode == this.root) {
+			return false;
+		}
+		return existingNode.isEmpty();
+	}
+
+	private Node findMostSpecificNodeFor(IJavaElement element) {
+		if (element == null) {
+			return this.root;
+		}
+
+		Node parentNode = findMostSpecificNodeFor(element.getParent());
+		Node child = parentNode.findChildFor(element);
+		if (child == null) {
+			return parentNode;
+		}
+		return child;
+	}
+
+	@Override
+	public IJavaElement[] getElements() {
+		int leaves = countLeafNodes();
+
+		IJavaElement[] result = new IJavaElement[leaves];
+		int insertions = this.root.gatherLeaves(result, 0);
+
+		assert insertions == leaves;
+
+		return result;
+	}
+
+	private int countLeafNodes() {
+		if (this.root.isEmpty()) {
+			return 0;
+		}
+		return this.root.countLeafNodes();
+	}
+
+	private Node findExactNode(IJavaElement element) {
+		if (element == null) {
+			return this.root;
+		}
+
+		Node parentNode = findExactNode(element.getParent());
+
+		if (parentNode == null) {
+			return null;
+		}
+
+		return parentNode.findChildFor(element);
+	}
+
+	@Override
+	public boolean remove(IJavaElement element) {
+		Node node = findExactNode(element);
+
+		if (node == null) {
+			return false;
+		}
+		node.clearChildren();
+		boolean returnValue = node.isEmpty();
+
+		List<Node> ancestors = new ArrayList<>();
+		findPath(ancestors, element);
+
+		IJavaElement currentElement = element;
+		int idx = ancestors.size();
+
+		while (--idx > 0 && currentElement != null) {
+			Node current = ancestors.get(idx);
+			Node parent = ancestors.get(idx - 1);
+
+			if (current.isEmpty()) {
+				parent.removeChild(currentElement);
+			} else {
+				break;
+			}
+			currentElement = currentElement.getParent();
+		}
+
+		return returnValue;
+	}
+
+	private void findPath(List<Node> ancestors, IJavaElement element) {
+		if (element == null) {
+			ancestors.add(this.root);
+			return;
+		}
+
+		findPath(ancestors, element.getParent());
+
+		Node last = ancestors.get(ancestors.size() - 1);
+		Node next = last.findChildFor(element);
+		if (next != null) {
+			ancestors.add(next);
+		}
+	}
 }
